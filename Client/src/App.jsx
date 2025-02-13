e53062af327bf6deb46734d60bbe7d2f8760f815
mirmudasir692 <mirmudasir692@gmail.com>
2025-02-13 22:36:32 +0530

    added some packages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,18 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home"; // Create a Home component
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        
         {/* Login Page */}
         <Route path="/login" element={<Login />} />
         {/* Home Page */}
         <Route path="/" element={<Home />} />
-
       </Routes>
     </Router>
   );
