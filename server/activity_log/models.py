61fba07ed9ae1e696993104a48cf2e39f99e110f
mirmudasir692 <118063314+mirmudasir692@users.noreply.github.com>
2025-02-15 23:30:31 +0530

    Update models.py

--- server/activity_log/models.py
+++ server/activity_log/models.py
@@ -1,14 +1,14 @@
-from mongoengine import Document, StringField, DateTimeField, ReferenceField
-from datetime import datetime, timezone
+# from mongoengine import Document, StringField, DateTimeField, ReferenceField
+# from datetime import datetime, timezone
 
 
-class AuditLog(Document):
-    meta = {"collection": "audit_log"}
-    model_name = StringField(required=True)
-    document_id = ReferenceField(required=True)
-    action = StringField(required=True, choices=["created", "updated", "deleted"])
-    timestamp = DateTimeField(default=datetime.now(timezone.utc))
-    changes = StringField()
+# class AuditLog(Document):
+#     meta = {"collection": "audit_log"}
+#     model_name = StringField(required=True)
+#     document_id = ReferenceField(required=True)
+#     action = StringField(required=True, choices=["created", "updated", "deleted"])
+#     timestamp = DateTimeField(default=datetime.now(timezone.utc))
+#     changes = StringField()
 
-    def __str__(self):
-        return f"{self.action} {self.model_name} {self.document_id}"
+#     def __str__(self):
+#         return f"{self.action} {self.model_name} {self.document_id}"
